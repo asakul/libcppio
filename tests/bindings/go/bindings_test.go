8e6f1973917217bd329cf52dc149ddf8971d8fec
Denis Tereshkin <denis@kasan.ws>
2016-08-11 07:24:20 +0700

    Go bindnigs: test fix

--- tests/bindings/go/bindings_test.go
+++ tests/bindings/go/bindings_test.go
@@ -7,6 +7,7 @@ import ("testing"
 	"cppio")
 
 func simpleTest(t *testing.T, endpoint string) {
+	server_ready := make(chan int)
 	var wg sync.WaitGroup
 	wg.Add(2)
 	data := ([]byte)("foobar-foobar-foobar-0123456789")
@@ -18,7 +19,9 @@ func simpleTest(t *testing.T, endpoint string) {
 			return
 		}
 		defer s.Close()
+		server_ready <- 1
 		c := s.WaitConnection(1000)
+		defer c.Close()
 		buffer := make([]byte, 256, 256)
 		rd, err := c.Read(buffer)
 		if err != nil {
@@ -38,6 +41,7 @@ func simpleTest(t *testing.T, endpoint string) {
 
 	client := func () {
 		defer wg.Done()
+		<-server_ready
 		c, err := cppio.CreateClient(endpoint)
 		if err != nil {
 			t.Fail()
@@ -58,5 +62,15 @@ func simpleTest(t *testing.T, endpoint string) {
 
 func TestSimpleTcpIo(t *testing.T) {
 	simpleTest(t, "tcp://127.0.0.1:5510")
+	simpleTest(t, "tcp://127.0.0.1:5511")
 }
 
+func TestSimpleInprocIo(t *testing.T) {
+	simpleTest(t, "inproc://foobar")
+	simpleTest(t, "inproc://foobar2")
+}
+
+func TestSimpleLocalIo(t *testing.T) {
+	simpleTest(t, "local:///tmp/foobar")
+	simpleTest(t, "local:///tmp/foobar2")
+}
